0752ab195d1e3b19ea0d7a25da6f64809537316a
simon <350400115@qq.com>
2020-07-01 14:58:59 +0800

    login

--- src/service/index.js
+++ src/service/index.js
@@ -4,11 +4,11 @@ import { http } from '@/config/util'
 export const getAuthAppId = (authType = 1) => http.get('https://nb-oa.lianlianlvyou.com/v2/wx/auth?platForm=' + authType)
 
 // 账号密码登录
-export const accountLogin = (userName, password) => http.post('/common/login/loginByPassword', { phoneNumber: userName, password })
+export const accountLogin = (userName, password) => http.post('/common/open/login/loginByPassword', { phoneNumber: userName, password })
 
-export const loginWithCode = (code, platform) => http.post('/common/login/wechat/login', { code, platform })
+export const loginWithCode = (code, platform) => http.post('/common/open/login/wechat/login', { code, platform })
 
-export const wxBind = (userName, password, uid) => http.post('/common/login/wechat/loginByToken', { phoneNumber: userName, password, uid })
+export const wxBind = (userName, password, uid) => http.post('/common/open/login/wechat/loginByToken', { phoneNumber: userName, password, uid })
 
 // 获取菜单和用户信息
 export const getUserInfo = () => http.get('/common/login/getUserInfo')
