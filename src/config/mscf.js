3fc800158d16847e85976f15dd99f0d9df7f5f75
Jianquan Yu <1029485996@qq.com>
2020-07-13 18:48:11 +0800

    测试数据

--- src/config/mscf.js
+++ src/config/mscf.js
@@ -20,9 +20,9 @@ mscf.on('toast.warning', (e) => {
 })
 
 // 某个系统想跳转到其他子系统
-mscf.on('redirect', (data) => {
-  const { target, page, params, query } = data
-  console.log(data)
+mscf.on('redirect', (e) => {
+  const { target, page, params, query } = e.data
+  console.log(e.data)
   const system = getSystem(target)
   if (!system) return console.error(`Unregistered system: "${target}"!`)
   let targetPage = system.pages[page]
