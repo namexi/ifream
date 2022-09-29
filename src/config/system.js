cacda7e8ff0e675c041894efd375c12dd4774d65
hanxi <>
2022-09-29 15:23:40 +0800

    feat:子系统路径修改

--- src/config/system.js
+++ src/config/system.js
@@ -566,7 +566,7 @@ export const subSysTemConfig = {
   },
   leecode: {
     name: 'leecode',
-    url: `${prefixUrl}/lowcode/index.html`,
+    url: `${prefixUrl}/leecode/index.html`,
     pages: {
       lowcodeEditor: '/lowcode/editor'
     }
