12574814f1d9bbf65bd3a1824fd42e4df46005a7
hanxi <>
2022-10-28 13:35:32 +0800

    feat：增加抖音poi管理二级页面

--- src/config/system.js
+++ src/config/system.js
@@ -475,7 +475,10 @@ export const subSysTemConfig = {
       boardProductHost: '/douJiaManage/boardProductHost',
       boardShops: '/douJiaManage/boardShops',
       boardProduct: '/douJiaManage/boardProduct',
-      dataBoardAdmin: '/douJiaManage/dataBoardAdmin'
+      dataBoardAdmin: '/douJiaManage/dataBoardAdmin',
+      spuPlusInfo: '/spuPlusInfo',
+      spuPlusManag: '/spuPlusManag',
+      addSpuPlus: '/addSpuPlus'
     }
   },
   merchant: {
