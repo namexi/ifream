5ecd0282eff3c15c399109ce5101131d27a08aac
hanxi <>
2022-11-24 17:40:02 +0800

    feat：增加直营crm详情页

--- src/config/system.js
+++ src/config/system.js
@@ -595,7 +595,15 @@ export const subSysTemConfig = {
   },
   zycrm: {
     name: 'zycrm',
-    url: `${prefixUrl}/zycrm/index.html`
+    url: `${prefixUrl}/zycrm/index.html`,
+    pages: {
+      businessDeta: '/crm/business/details/info',
+      businessInfo: '/crm/business/details/info',
+      kpInfo: '/crm/business/details/kpInfo',
+      followInfo: '/crm/business/details/followInfo',
+      storeInfo: '/crm/business/details/storeInfo',
+      contractInfo: '/crm/business/details/contractInfo'
+    }
   }
 }
 
